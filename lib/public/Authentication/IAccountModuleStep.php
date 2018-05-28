200fc8cd4cbf3dee1286591851c17b25849fff83
Jörn Friedrich Dreyer <jfd@butonic.de>
2018-05-28 09:07:51 +0200

    poc

---
+++ lib/public/Authentication/IAccountModuleStep.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace OCP\Authentication;
+
+use OCP\AppFramework\Http\RedirectResponse;
+
+/**
+ * Interface IAccountModuleStep
+ *
+ * @package OCP\Authentication
+ * @since 10.0.9
+ */
+interface IAccountModuleStep {
+
+	/**
+	 *
+	 * @since 10.0.9
+	 *
+	 * @return RedirectResponse
+	 */
+	public function getRedirectResponse();
+}
